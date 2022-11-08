89104bbf91917c51fa673e307c2e14ee2e4246ac
dmstepanuik <0669218646@ukr.net>
2022-11-08 17:46:21 +0200

    commit 4

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,13 +2,18 @@ import Navbar from './layouts/Navbar/Navbar';
 import { Routes, Route } from 'react-router-dom';
 import Home from './pages/Home/Home';
 import Movies from './pages/Movies/Movies';
+
+const BASE_PATH = 'goit-react-hw-05-movies';
+
 export const App = () => {
   return (
     <>
       <Navbar />
       <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/movies" element={<Movies />} />
+        <Route path={`/${BASE_PATH}`}>
+          <Route path="" element={<Home />} />
+          <Route path="movies" element={<Movies />} />
+        </Route>
       </Routes>
     </>
   );
